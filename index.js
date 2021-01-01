1d410bfa66e7bc88a51cd303e7c64be956645f3b
Levi Wong <lei.huang@binance.com>
2021-01-01 10:25:34 +0800

    fix: delay should work on all messages

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-
-export default (duration)=>source=>{
-    return (type,sink)=>{
-        if(type !== 0) return
-        source(0,(t,d)=>{
-            if(t !== 1) return sink(t,d)
-            let id = setTimeout(()=>{
-                clearTimeout(id)
-                sink(1,d)
-            },duration)
-        })
-    }
-}
+export default (duration) => (source) => {
+  return (type, sink) => {
+    if (type !== 0) return;
+    source(0, (t, d) => {
+      let id = setTimeout(() => {
+        clearTimeout(id);
+        sink(t, d);
+      }, duration);
+    });
+  };
+};
